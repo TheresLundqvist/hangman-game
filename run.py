a68f0d75c9a1642821584956098b2b3a622b29aa
TheresLundqvist <theres.lundqvist@outlook.com>
2022-11-10 22:38:37 +0000

    AdAdd if statement to show user the correctly guessed letters otherwise hide with a *

--- run.py
+++ run.py
@@ -30,6 +30,13 @@ def play_game():
 
     # get input from user as long as letters_in_word is greater then 0
     while len(letters_in_word) > 0:
+        # each used letter shown as a string seperated by whitespace
+        print("You have used these letters: ", " ".join(guessed_letters))
+
+        # show correctly guessed letters in word otherwise hide letters with a *
+        word_list = [letter if letter in guessed_letters else "*" for letter in word]
+        print("The current word is: ", " ".join.(word_list))
+
         user_input = input("Guess a letter: ").upper()
         # if user input is a valid letter and has not been used yet
         # then add the guessed letter to guessed_letter set.
